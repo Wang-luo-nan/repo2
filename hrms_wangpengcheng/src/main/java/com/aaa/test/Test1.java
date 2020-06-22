ac2f53c361a8679015573373c373077fe95378c5
wang <254548078@qq.com>
2020-06-22 18:53:16 +0800

    增加一个测试类

---
+++ hrms_wangpengcheng/src/main/java/com/aaa/test/Test1.java
@@ -0,0 +1,13 @@
+package com.aaa.test;
+
+/**
+ * @program: hrms_wangpengcheng
+ * @description:
+ * @author: Mr.Wang
+ * @create: 2020-06-22 18:51
+ **/
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
